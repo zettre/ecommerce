bb6181be82c749004872b6459221c066f8f763fe
zettre <vaibhav.singhh1007@gmail.com>
2016-11-14 13:51:20 +0530

    Connected to database

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ var UserSchema = new Schema({
 UserSchema.pre('save',function(next){
 	var user=this;
 	if(!user.isModified('password')) return next();
-	bcrypt.getSalt(10,function(err,salt){
+	bcrypt.genSalt(10,function(err,salt){
 		if(err) return next(err);
 		bcrypt.hash(user.password,salt,null,function(err,hash){
 			if(err) return next(err);
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var User = require('./models/user');
+
 var app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect('mongodb://root:Rankerspoint1007@ds151917.mlab.com:51917/ecommerce',function(err){
 	if(err)
@@ -16,11 +21,18 @@ mongoose.connect('mongodb://root:Rankerspoint1007@ds151917.mlab.com:51917/ecomme
 	}
 });
 
+app.post('/create-user',function(req,res,next){
+	var user=new User();
+	user.profile.name=req.body.name;
+	user.password=req.body.password;
+	user.email=req.body.email;
+	user.save(function(err){
+		if(err) return next(err);
+		res.json("User created!"+user.profile.name+"--"+user.email+"--"+user.password);
+	});
+});
+
 app.listen(7777,function(err){
 	if(err) throw err;
 	console.log("Server is listening on port 7777");
-});
-
-app.get('/',function(req , res) {
-	res.json("Hi I am a web app!");
 });
